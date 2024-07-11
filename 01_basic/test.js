e50acedcbc6a638ba1f8bed69328938eb80d4b52
mynkchats <142708150+mynkchats@users.noreply.github.com>
2024-07-11 14:28:13 +0000

    array2

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,7 @@
-console.log("Mayank")
+const marvel = ["ironman", "thor", "spidy"];
+const dc_hero = ["superman", "batman", "flash"]
+
+marvel.push(dc_hero)
+console.log(marvel);
+console.log(marvel[3][2]);
+
